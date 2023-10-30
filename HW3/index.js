893908bab71ca53e98eb6ba2fba33910d4fe42fe
liangweihan <144086846+liangweihan@users.noreply.github.com>
2023-10-31 01:45:39 +0800

    hw

--- HW3/index.js
+++ HW3/index.js
@@ -1,27 +1,32 @@
 const btnEl = document.getElementById("calculate");
-const firstdividendInput = document.getElementById("first dividend");
-const seconddividenddInput = document.getElementById("second dividend");
-const thirddividendInput = document.getElementById("third dividend");
-const fourthdividendInput = document.getElementById("fourth dividend");
-const fifthdividendInput = document.getElementById("fifth dividend");
+const firstdividendInput = document.getElementById("first-dividend");
+const seconddividendInput = document.getElementById("second-dividend");
+const thirddividendInput = document.getElementById("third-dividend");
+const fourthdividendInput = document.getElementById("fourth-dividend");
+const fifthdividendInput = document.getElementById("fifth-dividend");
 
 const cheappriceSpan = document.getElementById("cheapprice");
 const reasonablepriceSpan = document.getElementById("reasonableprice");
 const expensivepriceSpan = document.getElementById("expensiveprice");
 
 function calculateprice() {
-  const firstdividendvalue = firstdividendInput.value;
-  const seconddividenddvalue = seconddividenddInput.value;
-  const thirddividendvalue = thirddividendInput.value;
-  const fourthdividendvalue = fourthdividendInput.value;
-  const fifthdividendvalue = fifthdividendInput.value;
+  const firstdividendvalue = parseFloat(firstdividendInput.value);
+  const seconddividendvalue = parseFloat(seconddividendInput.value);
+  const thirddividendvalue = parseFloat(thirddividendInput.value);
+  const fourthdividendvalue = parseFloat(fourthdividendInput.value);
+  const fifthdividendvalue = parseFloat(fifthdividendInput.value);
 
-  const cheappriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/7;
+  const averageDividend = (firstdividendvalue + seconddividendvalue + thirddividendvalue + fourthdividendvalue + fifthdividendvalue) / 5;
+
+  const cheappriceValue = (averageDividend * 100) / 7;
   cheappriceSpan.innerText = cheappriceValue.toFixed(2);
-  const reasonablepriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/5;
+
+  const reasonablepriceValue = (averageDividend * 100) / 5;
   reasonablepriceSpan.innerText = reasonablepriceValue.toFixed(2);
-  const cheapppriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/3;
-  cheappriceSpan.innerText = cheapppriceValue.toFixed(2);
+
+  const expensivepriceValue = (averageDividend * 100) / 3;
+  expensivepriceSpan.innerText = expensivepriceValue.toFixed(2);
 }
 
-btnEl.addEventListener("click", calculateTotal);
+btnEl.addEventListener("click", calculateprice);
+
