c51632e63fb260a383029fbd92f8f2f05ad581ff
liangweihan <144086846+liangweihan@users.noreply.github.com>
2023-10-31 00:43:22 +0800

    hw4

--- HW3/index.js
+++ HW3/index.js
@@ -1,13 +1,27 @@
 const btnEl = document.getElementById("calculate");
-const billInput = document.getElementById("bill");
-const tipInput = document.getElementById("tip");
-const totalSpan = document.getElementById("total");
+const firstdividendInput = document.getElementById("first dividend");
+const seconddividenddInput = document.getElementById("second dividend");
+const thirddividendInput = document.getElementById("third dividend");
+const fourthdividendInput = document.getElementById("fourth dividend");
+const fifthdividendInput = document.getElementById("fifth dividend");
 
-function calculateTotal() {
-  const billValue = billInput.value;
-  const tipValue = tipInput.value;
-  const totalValue = billValue * (1 + tipValue / 100);
-  totalSpan.innerText = totalValue.toFixed(2);
+const cheappriceSpan = document.getElementById("cheapprice");
+const reasonablepriceSpan = document.getElementById("reasonableprice");
+const expensivepriceSpan = document.getElementById("expensiveprice");
+
+function calculateprice() {
+  const firstdividendvalue = firstdividendInput.value;
+  const seconddividenddvalue = seconddividenddInput.value;
+  const thirddividendvalue = thirddividendInput.value;
+  const fourthdividendvalue = fourthdividendInput.value;
+  const fifthdividendvalue = fifthdividendInput.value;
+
+  const cheappriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/7;
+  cheappriceSpan.innerText = cheappriceValue.toFixed(2);
+  const reasonablepriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/5;
+  reasonablepriceSpan.innerText = reasonablepriceValue.toFixed(2);
+  const cheapppriceValue = ((firstdividendvalue+seconddividenddvalue+thirddividendvalue+fourthdividendvalue+fifthdividendvalue)/5)*100/3;
+  cheappriceSpan.innerText = cheapppriceValue.toFixed(2);
 }
 
 btnEl.addEventListener("click", calculateTotal);
